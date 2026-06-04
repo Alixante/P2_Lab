2479e80d8357383cc62026e4eae243a8db09a53c
LightStorm06 <llufepi@gmail.com>
2026-06-04 18:02:04 +0200

    Render medio done

--- src/game.cpp
+++ src/game.cpp
@@ -2,6 +2,7 @@
 #include <random>
 #include "graphics.h"
 #include "candy.h"
+#include "board.h"
 
 Game::Game()
 {
@@ -15,16 +16,42 @@ Game::~Game()
 
 void Game::update(const Controller& controller)
 {
+    static Board tablero;
+    static int cursorX = 0;
+    static int cursorY = 0;
+    static int result = 0;
+
     // Implement your code here
+    if (controller.isDownPressed())
+    {
+        if (cursorY < tablero.getHeight() - 1)
+        {
+            ++cursorY;
+        }
+    }
+    if (controller.isUpPressed())
+    {
+        if (cursorY > 0) //No tienes sentido, odio mi vida
+    }
    
 }
 
 void Game::render(GraphicManager& graphics)
 {
     // Implement your code here
-    
-    // Note: the following code exhibits the main graphic library features
-    // Board: border [draw rectangles] and a single piece of candy
+    std::random_device rd; //Genera semilla aleatoria
+    std::mt19937 gen(rd()); //Cargar semilla
+
+    std::uniform_int_distribution<int> dist(0, (int)(CandyType::COUNT) - 1); //Parametros de que a que Candy, 0-5
+
+    CandyType caramelos[3]; 
+
+
+    for (int i = 0; i < 3; i++)
+    {
+        caramelos[i] = (CandyType)(dist(gen)); //Ejecuta rand()
+    }
+
     const int board_size = 10;
     const int board_padding = 3;
     graphics.drawRectangle(
@@ -32,16 +59,18 @@ void Game::render(GraphicManager& graphics)
         CANDY_IMAGE_WIDTH * board_size,
         CANDY_IMAGE_HEIGHT * board_size,
         5, 150, 150, 150);
-    // Board: place a candy piece
-    graphics.drawImage(Candy(CandyType::TYPE_PURPLE).getResourceName(),
-        CANDY_IMAGE_WIDTH * 3,
-        CANDY_IMAGE_HEIGHT * 3);
+
+    for (int j = 0; j < 3; j++)
+    {
+        graphics.drawImage(Candy(caramelos[j]).getResourceName(), CANDY_IMAGE_WIDTH * 3, CANDY_IMAGE_HEIGHT * 3);
+    }
+
     // Title [draw images]
     graphics.drawImage("img/logo_small.png", 10, 10);
     // Score and footer [draw text]
     graphics.drawText("Movement: [Up] [Down] [Left] [Right]  --  "
-                      "Buttons: [Q] [W] [E]  --  Exit [ESC]",
-                      25, 700, 20, 100, 100, 100);
+        "Buttons: [Q] [W] [E]  --  Exit [ESC]",
+        25, 700, 20, 100, 100, 100);
     graphics.drawText("Score: ", 450, 10, 70, 125, 200, 125);
 }
 
